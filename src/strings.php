c91f597e0d31dd6f07dd22625936b9f0382d0d38
Peter <ppkrauss@gmail.com>
2017-07-09 10:09:46 -0300

    add draft desacent

--- src/strings.php
+++ src/strings.php
@@ -434,6 +434,14 @@ function autolink_email($text, $tagfill=''){
 		return $buffer;
 }
 
+function desacent($string){ // ugly but reliable, from https://pt.stackoverflow.com/a/49655/4186
+  return preg_replace(
+	["/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"],
+	explode(" ","a A e E i I o O u U n N"),
+	$string
+   );
+}
+
 //
 // END:auto-linking
 // // // // // // // // // // // // //
